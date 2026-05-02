d2151f1ab46d83c80236b1c012702bf595c70fbd
rashed ali <rashedalidesigner@gmail.com>
2026-05-02 13:41:30 +0600

    done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/my-portfolio-project/",
+  base: "my_portfilio",
   plugins: [tailwindcss(), react()],
 })
